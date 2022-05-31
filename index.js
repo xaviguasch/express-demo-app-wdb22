859e67f2dd2dfba53c664eb240c0632cd1b14562
xaviguasch <xaviguasch@gmail.com>
2022-05-31 16:39:26 +0200

    played with some Express basic routing

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 const express = require('express')
 const app = express()
 
-app.use((req, res) => {
-  console.log('We got a new request!')
-  // res.send('Hello, we got your request!')
-  // res.send({ color: 'red' })
-  res.send('<h1>This is my webpage</h1>')
+// app.use((req, res) => {
+//   console.log('We got a new request!')
+//   // res.send('Hello, we got your request!')
+//   // res.send({ color: 'red' })
+//   res.send('<h1>This is my webpage</h1>')
+// })
+
+app.get('/', (req, res) => {
+  res.send('This is the homepage!')
+})
+app.get('/cats', (req, res) => {
+  res.send('MEOW!!!!')
+})
+app.post('/cats', (req, res) => {
+  res.send('This is a POST request to /cats')
+})
+app.get('/dogs', (req, res) => {
+  res.send('WOOOOF!!!!')
+})
+
+app.get('*', (req, res) => {
+  res.send(`I don't know that path`)
 })
 
 app.listen(3000, () => {
