71c714f4cf4ea2d0de460c3f099018b96bacef63
xaviguasch <xaviguasch@gmail.com>
2022-05-31 16:19:12 +0200

    responed to requests to the server

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 const express = require('express')
 const app = express()
-console.dir(app)
+
+app.use((req, res) => {
+  console.log('We got a new request!')
+  // res.send('Hello, we got your request!')
+  // res.send({ color: 'red' })
+  res.send('<h1>This is my webpage</h1>')
+})
+
+app.listen(3000, () => {
+  console.log('LISTENING ON PORT 3000!!!!')
+})
